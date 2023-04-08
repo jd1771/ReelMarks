ed781b4e6927fcd59d43166991e010aaed013170
Jas Dhaul <jasdhaul0@gmail.com>
2023-04-08 15:18:42 -0700

    Started implementing youtube-api

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { google } from "googleapis";
 
 dotenv.config();
 
 const app = express();
 
+// Create YouTube client with the API key
+const youtube = google.youtube({
+    version: "v3",
+    auth: process.env.YOUTUBE_API_KEY,
+});
+
+app.get("/api/:vidID", async (req, res) => {
+    const id = req.params.vidID;
+
+    const videoParams = {
+        part: "snippet,contentDetails",
+        id: id,
+    };
+
+    try {
+        // Call the videos.list API endpoint with the specified parameters
+        const response = await youtube.videos.list(videoParams);
+
+        // send response data to client with status code
+        res.status(200).send(response.data);
+    } catch (error) {
+        // send error message to client with status code
+        res.status(500).send(error.message);
+    }
+});
+
 app.listen(3000, () => {
-  console.debug("App listening on :3000");
+    console.debug("App listening on :3000");
 });
