9715b215bf7d1d0cbee9bb8bbca88552a980f7e8
Jas Dhaul <jasdhaul0@gmail.com>
2023-04-21 10:11:14 -0700

    Integrated Redis caching for transcripts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { getVideoInfo, getVideoLength } from "./common/getYoutubeData.js";
 import { getYoutubeTranscript } from "./common/getYoutubeTranscript.js";
 import { createBatches } from "./common/createBatches.js";
 import { Configuration, OpenAIApi } from "openai";
+import redis from "redis";
 
 dotenv.config();
 
@@ -15,8 +16,34 @@ const openAIConfiguration = new Configuration({
 
 const openai = new OpenAIApi(openAIConfiguration);
 
+const redisClient = redis.createClient({
+    host: "localhost",
+    port: 6379,
+});
+
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log("Redis client connected");
+    } catch (error) {
+        console.error("Failed to connect to Redis:", error);
+        process.exit(1);
+    }
+})();
+
 app.get("/api/:vidID", async (req, res) => {
     const videoId = req.params.vidID;
+
+    // Check if the videoID exists in Redis
+    try {
+        const result = await redisClient.get(videoId);
+        if (result) {
+            console.log("Retrieved from Redis");
+            return res.send(JSON.parse(result));
+        }
+    } catch (error) {
+        return res.send("Error retrieving video");
+    }
 
     const videoInfo = await getVideoInfo(videoId, process.env.YOUTUBE_API_KEY);
     if (videoInfo.error) {
@@ -51,7 +78,7 @@ app.get("/api/:vidID", async (req, res) => {
             max_tokens: 2000,
             stream: false,
         });
-        //console.log(prompt + JSON.stringify(batch));
+
         let timestampText = response.data.choices[0].text;
 
         // Remove any new lines from the text
@@ -70,13 +97,18 @@ app.get("/api/:vidID", async (req, res) => {
         timestamps.push(timeText);
     }
 
+    // Create data object
     const data = {
         title: videoInfo.title,
         duration: videoInfo.duration,
         transcript: cleanedTranscript,
+        timestamps: timestamps,
     };
 
-    res.send(timestamps);
+    // Save the transcription in Redis
+    await redisClient.set(videoId, JSON.stringify(timestamps));
+
+    res.send(data.timestamps);
 });
 
 app.listen(3000, () => {
